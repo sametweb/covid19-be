1bf6d97b64558b133a0f7a4f4e8c172892a21abe
sametweb <sametmutevellioglu@gmail.com>
2021-03-13 00:18:47 -0800

    https missing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const api = require("./api.js");
 
 var corsOptions = {
-  origin: "http://covid19daily.info",
+  origin: "https://covid19daily.info",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
